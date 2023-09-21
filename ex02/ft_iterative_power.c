4b149e2ba7d133a1d85d6d605f56da2437ca3e79
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2023-09-21 20:35:09 +0200

    corr retry01

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -17,15 +17,15 @@ int	ft_iterative_power(int nb, int power)
 	int	res;
 
 	res = nb;
-	if ((power == 0) && (nb == 0))
-		return (1);
-	if (power == 0)
-		return (1);
 	while (power > 1)
 	{
 		res *= nb;
-		power = power - 1;
+		power--;
 	}
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
 	return (res);
 }
 
--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -14,13 +14,16 @@
 
 int	ft_recursive_power(int nb, int power)
 {
-	if ((nb == 0) && (nb == 0))
-		return (0);
+	int	result;
+
+	result = nb;
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, power - 1));
 	if (power == 0)
 		return (1);
-	if (power == 1)
-		return (nb);
-	return (nb * ft_recursive_power(nb, power - 1));
+	if (power < 0)
+		return (0);
+	return (result);
 }
 
 /*int	main(void)
